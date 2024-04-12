68a856999dea31a9ba4120c4350580053ec6e59f
kyleconsebido <123555531+kyleconsebido@users.noreply.github.com>
2024-04-12 22:46:09 +0800

    add realtime updates to room

--- src/composables/useRoom.ts
+++ src/composables/useRoom.ts
@@ -1,10 +1,11 @@
 import type { Room } from '@/schema'
-import { ref } from 'vue'
-import { doc, getDoc } from 'firebase/firestore'
+import { onUnmounted, ref } from 'vue'
+import { type Unsubscribe, doc, onSnapshot } from 'firebase/firestore'
 import { db } from '@/firebase'
 import { Collection } from '@/schema'
 
 export default ({ roomId, roomData }: { roomId?: Room['id']; roomData?: Room }) => {
+  let unsub: Unsubscribe | null = null
   const room = ref<Room | null>(null)
   const loading = ref(false)
   const error = ref<Error | null>(null)
@@ -14,20 +15,23 @@ export default ({ roomId, roomData }: { roomId?: Room['id']; roomData?: Room }) 
       throw new TypeError('Pass either a roomId or roomData')
     }
 
-    let newRoomData = roomData
-
-    if (!newRoomData) {
-      const roomDoc = await getDoc(doc(db, Collection.ROOMS, roomId as string))
-
-      if (!roomDoc.exists()) {
-        throw new Error('Room does not exist')
-      }
-
-      newRoomData = roomDoc.data() as Room
-      newRoomData.id = roomDoc.id
+    if (roomData) {
+      room.value = { ...roomData }
     }
 
-    room.value = { ...newRoomData }
+    if (unsub) unsub()
+
+    unsub = onSnapshot(doc(db, Collection.ROOMS, roomId as string), (roomDoc) => {
+      if (!roomDoc.exists()) {
+        error.value = new Error('Room does not exist')
+        unsub?.()
+        return
+      }
+
+      roomData = roomDoc.data() as Room
+      roomData.id = roomDoc.id
+      room.value = { ...roomData }
+    })
   }
 
   const fetchRoom = async () => {
@@ -40,6 +44,8 @@ export default ({ roomId, roomData }: { roomId?: Room['id']; roomData?: Room }) 
   }
 
   fetchRoom()
+
+  onUnmounted(() => unsub?.())
 
   return {
     room,
