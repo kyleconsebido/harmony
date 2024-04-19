fbe33e032b25a7c842380c63d905bb4a86e69990
kyleconsebido <123555531+kyleconsebido@users.noreply.github.com>
2024-04-20 00:04:47 +0800

    add styles for messages

--- src/components/RoomMessages.vue
+++ src/components/RoomMessages.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
 import type { Room } from '@/schema'
 import { ref } from 'vue'
+import useAuth from '@/composables/useAuth'
 import useMessages from '@/composables/useMessages'
 import IconPlane from './icons/IconPlane.vue'
+import ProfileButton from './ProfileButton.vue'
+import LoadingDots from './LoadingDots.vue'
 
 interface Props {
   room: Room
@@ -10,29 +13,64 @@ interface Props {
 
 const { room } = defineProps<Props>()
 
+const { user } = useAuth()
+
 const { messages, sendMessage } = useMessages(room)
 
 const input = ref('')
 
+const getProfileBtnPos = (userId: string, index: number) => {
+  let position: InstanceType<typeof ProfileButton>['position'] =
+    userId === user.value?.uid ? 'left' : 'right'
+
+  if (index === 0) {
+    position = 'top-' + position
+  }
+
+  return position as InstanceType<typeof ProfileButton>['position']
+}
+
 const handleSend = () => {
   if (!input.value) return
 
-  input.value = ''
-  
   sendMessage(input.value)
+
+  input.value = ''
 }
 </script>
 
 <template>
   <div class="messages">
     <div class="chat">
-      <div v-for="msg of messages" :key="msg.id">
-        <p>{{ msg.message }}</p>
-        <span> - {{ msg.userName }} ~ {{ msg.sending }}</span>
+      <div
+        v-for="(msg, i) of messages"
+        :key="msg.id"
+        :class="{ sent: msg.userId === user?.uid }"
+        class="message"
+      >
+        <div class="name-container">
+          <div class="name">{{ msg.userName }}</div>
+        </div>
+        <div class="bubble-container">
+          <ProfileButton
+            :user-id="msg.userId"
+            :display-name="msg.userName!"
+            :position="getProfileBtnPos(msg.userId, i)"
+            class="msg-profile"
+            btn-class="msg-profile-btn"
+          />
+          <p class="bubble">{{ msg.message }}</p>
+          <LoadingDots v-if="msg.sending" class="sent-loading" />
+        </div>
       </div>
     </div>
     <form class="send-chat" @submit.prevent="handleSend">
-      <input v-model.trim="input" :placeholder="`Message ${room.name}`" />
+      <textarea
+        v-model.trim="input"
+        :placeholder="`Message ${room.name}`"
+        @keypress.exact.enter.prevent="handleSend"
+        @keypress.shift="null"
+      ></textarea>
       <button><IconPlane /></button>
     </form>
   </div>
@@ -46,6 +84,10 @@ const handleSend = () => {
   grid-template-rows: 1fr var(--send-chat-height);
 }
 
+.message {
+  margin-right: calc(var(--room-padding) - 3px);
+}
+
 .chat {
   padding-left: var(--room-padding);
   position: relative;
@@ -53,6 +95,7 @@ const handleSend = () => {
   display: flex;
   flex-direction: column-reverse;
   margin-right: 3px;
+  gap: 0.5em;
 
   &::-webkit-scrollbar-track {
     margin-top: 6px;
@@ -61,16 +104,80 @@ const handleSend = () => {
   }
 }
 
+.name-container {
+  display: flex;
+
+  &::before {
+    content: '';
+    width: calc(32px + var(--room-padding) / 2);
+  }
+
+  .name {
+    font-size: 0.8rem;
+    color: var(--color-text-dark-mute);
+  }
+}
+
+.bubble-container {
+  display: flex;
+  gap: 0.5em;
+
+  .bubble {
+    background-color: var(--color-received);
+    border-radius: var(--base-border-radius);
+    padding: 0.5em;
+    white-space: break-spaces;
+  }
+}
+
+.sent {
+  align-self: flex-end;
+
+  .name-container {
+    flex-direction: row-reverse;
+  }
+
+  .bubble-container {
+    flex-direction: row-reverse;
+
+    .bubble {
+      background-color: var(--color-sent);
+      color: var(--color-text-light);
+    }
+  }
+}
+
+.sent-loading {
+  font-size: 0.5rem;
+  align-self: center;
+  opacity: 0;
+  animation: fade-in 200ms 500ms forwards;
+}
+
+.msg-profile {
+  width: 32px;
+  height: 32px;
+  align-self: flex-end;
+}
+
+:deep(.msg-profile-btn) {
+  border-radius: 100%;
+  width: 32px;
+  height: 32px;
+}
+
 .send-chat {
   display: flex;
   column-gap: 0.5em;
   padding: var(--room-padding);
 
-  input {
+  textarea {
     border: none;
     outline: none;
+    resize: none;
     border-radius: var(--base-border-radius);
     padding: var(--room-padding);
+    padding-bottom: calc(var(--room-padding) - 2px);
     background-color: var(--color-input);
     color: var(--color-text-dark);
     font-family: inherit;
@@ -117,6 +224,15 @@ const handleSend = () => {
       width: 90%;
       height: 90%;
     }
+  }
+}
+
+@keyframes fade-in {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
   }
 }
 </style>
